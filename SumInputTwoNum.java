90d04eaf9f7c19146a2cafb93a4a4638c466fb98
Nayab-Codes <mdnayabhussain06@gmail.com>
2026-02-14 22:08:51 +0530

    Sum Of Two Numbers (Using Input)

---
+++ SumInputTwoNum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class SumInputTwoNum {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+       System.out.println("Enter First Number A:");
+       int A = sc.nextInt();
+       System.out.println("Enter Second Number B:");
+       int B = sc.nextInt();
+       int sum = A + B;
+       System.out.println(sum);
+    }
+}
