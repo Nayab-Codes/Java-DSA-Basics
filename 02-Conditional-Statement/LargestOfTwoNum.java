c6d00876a340eb1e65d1d8c5cbf8a9941d539c84
Nayab-Codes <mdnayabhussain06@gmail.com>
2026-02-15 23:45:21 +0530

    Largest Of Two Numbers (Using Conditional-Statement)

---
+++ 02-Conditional-Statement/LargestOfTwoNum.java
@@ -0,0 +1,12 @@
+public class LargestOfTwoNum {
+    public static void main(String [] args){
+        int A = 2;
+        int B = 5;
+        if (A > B){
+            System.out.println("A");
+        }
+        else{
+            System.out.println("B");
+        }
+    }
+}
