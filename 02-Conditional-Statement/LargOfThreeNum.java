9ad4d977e93d81b7af85227679b90cd516c06263
Nayab-Codes <mdnayabhussain06@gmail.com>
2026-02-16 21:38:32 +0530

    Largest Of Three Numbers (Using If-Else Statement)

---
+++ 02-Conditional-Statement/LargOfThreeNum.java
@@ -0,0 +1,17 @@
+public class LargOfThreeNum {
+    public static void main (String [] args){
+        int A = 1;
+        int B = 3;
+        int C = 6;
+        if(A > B){
+            System.out.println("A");
+        }
+        else
+            if(B > C){
+                System.out.println("B");
+            }
+            else{
+                System.out.println("C");
+            }
+    }
+}
