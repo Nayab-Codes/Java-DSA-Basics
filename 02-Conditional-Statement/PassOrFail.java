98b0865b1b37d1da0d699153008f2d467bdd2d89
Nayab-Codes <mdnayabhussain06@gmail.com>
2026-02-18 20:39:27 +0530

    Check Student Is Pass Or Fail (Using Ternary Operator)

---
+++ 02-Conditional-Statement/PassOrFail.java
@@ -0,0 +1,7 @@
+public class PassOrFail {
+    public static void main(String [] args){
+        int marks = 89;
+        String ReportCard = (marks >= 33)? "Pass" : "Fail";
+        System.out.println(ReportCard);
+    }
+}
