3afb4ee99ab80ef987fa542d086bc75d9622187e
Nayab-Codes <mdnayabhussain06@gmail.com>
2026-02-19 21:22:32 +0530

    Add a Java program to display day of week using (switch case)

---
+++ 02-Conditional-Statement/Week.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Week {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number Between 1 To 7 :");
+        int week = sc.nextInt();
+        switch(week){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid input! Please enter number between 1 To 7.");
+        }
+    }
+}
